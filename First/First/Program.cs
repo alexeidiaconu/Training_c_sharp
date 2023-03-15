4e15decbc01c2d114b1f93ff23711f883e55e7a8
alexdiaconu <alexei.diaconu@endava.com>
2023-03-15 13:09:47 +0200

    added read and write function

---
+++ First/First/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int BoxA;
+//BoxA = 35;
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Input Something");
+BoxA = int.Parse(Console.ReadLine());
+//String a = String.Create(BoxA);
+Console.WriteLine("Input was:" + BoxA);
+Console.WriteLine("Number was:" + BoxA);
