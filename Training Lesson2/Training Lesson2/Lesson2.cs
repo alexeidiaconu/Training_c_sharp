62e49c81391b03dceb17dd6529a72130ed978a8a
alexdiaconu <alexei.diaconu@endava.com>
2023-03-15 14:51:25 +0200

    Added Lesson2 Project

---
+++ Training Lesson2/Training Lesson2/Lesson2.cs
@@ -0,0 +1,32 @@
+﻿public class Lesson2
+{
+    private static void Main(string[] args)
+    {
+        readandwrite();
+
+
+
+
+    }
+
+    public static void readandwrite()
+    {
+        String BoxA;
+        int output;
+        //BoxA = 35;
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Input Something");
+        BoxA = Console.ReadLine();
+        if (int.TryParse(BoxA,out output) == true) {
+            Console.WriteLine("Input was:" + BoxA);
+            Console.WriteLine("Number was:" + BoxA);
+        }
+        else { 
+            Console.WriteLine("Incorrect Input!" + BoxA + " Please enter a number!");
+            readandwrite();
+                }
+        
+      
+    }
+
+}
